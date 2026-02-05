45a9d8c81fe4589bd56d709ff81f40130900b8bb
Felipe Andrada <felipe.andrada@gmail.com>
2026-02-05 00:00:47 -0300

    chore: setup PWA

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-restricted-globals */
+
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules/workbox-strategies
+// for more info on caching strategies.
+
+const CACHE_NAME = 'gym-timer-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/logo192.png',
+    '/logo512.png',
+    '/favicon.ico',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                    return null;
+                })
+            );
+        })
+    );
+});
